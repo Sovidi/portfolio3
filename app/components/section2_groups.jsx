8d5652bdb1e73ee05ba11098b25216d23ccfc6bf
Sovidi <lim132445@gmail.com>
2025-08-19 01:10:38 +0900

    기술스택 카드 active 매커니즘 수정

--- app/components/section2_groups.jsx
+++ app/components/section2_groups.jsx
@@ -8,7 +8,7 @@ const Section2_groups = ({ skillbox, className = "", tagId, idEdit, setSelectedI
         {skillbox?.map((item) => (
           <>
             <figure
-              className={`${item.id === tagId ? "active !z-[1000]" : ""}`}
+              className={`${item.id === tagId ? "active" : ""}`}
               onClick={(e) => {
                 console.log(item);
                 idEdit(e, item.id);
@@ -16,7 +16,7 @@ const Section2_groups = ({ skillbox, className = "", tagId, idEdit, setSelectedI
               }}
             >
               <div className="boxes" style={{ "background-image": `url(${item.src})` }}></div>
-              <div className="border-black popup popclose border-[1px] border-solid">
+              <div className={`${item.id === tagId ? "opacity-1 brp:translate-x-[0px] max-brp:translate-x-[-50%]" : "opacity-1 max-brp:select-none brp:translate-x-[1000px] max-brp:translate-x-[550%]"}  max-brp:translate-y-[-50%] max-brp:!top-[50%] max-brp:!left-[50%] absolute max-brp:fixed border-[black] popup popclose border-[1px] border-solid`}>
                 <p className="skillTitle">{item.name}</p>
                 <div style={{ "background-image": `url(${item.src})` }} className="popclose"></div>
                 <p className="popclose">{item.text}</p>
