4374464c20545b409da4cc9b80e36bbcbfd4a14c
Sovidi <lim132445@gmail.com>
2025-08-25 05:23:19 +0900

    데이터 컴포넌트 분리

--- app/components/Section3.js
+++ app/components/Section3.js
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from "react";
 import useMatchMedia from "./useMatchMedia";
+import { workbox } from "../datas/works";
+import parse from "html-react-parser";
 
 function Section3() {
   const [workSrc, setWorkSrc] = useState("./assets/portfolio.gif");
@@ -7,26 +9,6 @@ function Section3() {
   const scrollContainerRef = useRef(null);
   const boxsRef = useRef([]);
   const brp = useMatchMedia(`(max-width:1300px)`);
-
-  const workbox = [
-    { id: 0, expired: true, name: "Portfolio (Vue.js) *서버폐쇄", desc: "Vue.js로 제작된 포트폴리오 페이지 입니다.", src: "./assets/portfolio.gif", skills: "#Vue.js, #scss, #html, #MongoDB, #Express", href: ["https://port-0-portfolio2-6w1j2alm48bfok.sel5.cloudtype.app/"], githref: "https://github.com/Sovidi/portfolio2" },
-    { id: 1, expired: false, name: "Portfolio (Next.js)", desc: "현재 보고계신 Next.js 이식버전의 포트폴리오입니다.", src: "./assets/portfolio.gif", skills: "#Next.js, #scss, #html, #MongoDB, #Express", href: ["https://portfolio3-blue.vercel.app/"], githref: "https://github.com/Sovidi/portfolio3" },
-    { id: 2, expired: false, name: "Wonder place", desc: "원더플레이스 브랜드관 소개페이지 클론코딩", src: "./assets/wonderplace.png", skills: "#javascript, #scss, #html, #jQuery", href: ["https://sovidi.github.io/wonderplace_clone/"], githref: "https://github.com/Sovidi/wonderplace_clone" },
-    { id: 3, expired: false, name: "Everland", desc: "에버랜드 홈페이지 리뉴얼", src: "./assets/everland.png", skills: "#javascript, #scss, #html, #jQuery", href: ["https://sovidi.github.io/everland/"], githref: "https://github.com/Sovidi/everland" },
-    { id: 4, expired: false, name: "Scary House", desc: "귀신의 집, 공포 추리 미니 게임", src: "./assets/scaryhouse.png", skills: "#react, #scss, #html, #Express", href: ["https://sovidi.github.io/scary/"], githref: "https://github.com/Sovidi/scary" },
-    { id: 5, expired: false, name: "Movie Finder", desc: "MovieDB API 를 활용한 간단 무비 파인더", src: "./assets/moviefinder.gif", skills: "#react, #scss, #html", href: ["https://sovidi.github.io/movies/"], githref: "https://github.com/Sovidi/movies" },
-    { id: 6, expired: false, name: "BookFinder(개발중)", desc: "NaverBook RestAPI 를 활용한 서적 검색기", src: "./assets/bookfinder.gif", skills: "#react, #scss, #html", href: ["https://book-finder-fawn-kappa.vercel.app/"], githref: "https://github.com/Sovidi/BookFinder" },
-    { id: 7, expired: true, name: "toGrocery (MySQL) *서버폐쇄", desc: "시장 농산물 물가현황 간편확인 및 위치기반 매칭 어플", src: "./assets/togrocery.png", skills: "#next.js, #scss, #html, #MySQL", href: ["https://togrocery.vercel.app/"], githref: "https://github.com/Sovidi/togrocery" },
-    { id: 8, expired: false, name: "toGrocery (MongoDB)", desc: "장보러가자의 서버코드를 MongoDB(NoSQL)로 바꾼 버전입니다.", src: "./assets/togrocery.png", skills: "#next.js, #scss, #html, #MongoDB", href: ["https://to-grocery2.vercel.app/"], githref: "https://github.com/Sovidi/toGrocery2" },
-    { id: 9, expired: false, name: "petSalon", desc: "미용사 찾기 위치기반 서비스 어플", src: "./assets/petsalon.png", skills: "#react, #css, #html", href: ["https://pet-salon.vercel.app/"], githref: "https://github.com/Sovidi/petSalon" },
-    { id: 10, expired: false, name: "웅진 개정수학 e-book 컨텐츠", desc: "웅진 개정수학 컨텐츠로써, 2학년 2학기 컨텐츠 개발 담당,", src: "./assets/wj_math.png", skills: "#javascript, #css, #html, #jquery", href: ["https://wj-revision-math-exported.vercel.app/%EC%96%91%EC%82%B02/2/2-2-3/INDEX3.HTML"], githref: "https://github.com/Dalguiee/wj_revision_math_exported" },
-    { id: 15, expired: false, name: "2024 Tiving Festa 이벤트 페이지", desc: "Tiving 이벤트 페이지 입니다. 2일 정도의 시간안에 긴급히 배포하게된 프로젝트입니다.", src: "./assets/tving.png", skills: "#javascript, #css, #html, #jquery", href: ["https://dalguiee.github.io/tving_festa2024_exported/"], githref: "https://github.com/Dalguiee/tving_festa2024_exported" },
-    { id: 11, expired: false, name: "스마트올 중학", desc: "웅진 스마트올 중학 홈페이지 프론트 운영관리(2024년~)", src: "./assets/smart_all_middle.png", skills: "#javascript, #css, #html, #jquery", href: ["https://smartallmid.wjthinkbig.com/"] },
-    { id: 12, expired: false, name: "프리스비", desc: "애플 서드파티 마켓인 프리스비 홈페이지 프론트 운영관리(2024년~)", src: "./assets/frisbee.png", skills: "#javascript, #css, #html, #jquery", href: ["https://www.frisbeekorea.com/shop/main/main.do"] },
-    // { id: 11, name: "웅진 개정수학(선긋기 게임 컨텐츠)", desc: "게임 시스템 핵심코드 리펙토링 담당하였습니다.", src: "./assets/petsalon.png", skills: "#javascript, #css, #html, #jquery", href: "https://sovidi.github.io/exported_wj_revision_math/%EC%96%91%EC%82%B02/1/2-1-4/INDEX1.HTML", githref: "https://github.com/Sovidi/exported_wj_revision_math" },
-    { id: 13, expired: false, name: "미래엔 고등 중국어 e-book 컨텐츠", desc: "미래엔 고등과 중등 교과개정 e-book 컨텐츠 개발중 고등 중국어 담당 개발하였습니다. 중국어 컨텐츠 전체 개발 및 선긋기 게임 컨텐츠를 위한 핵심기능 스크립트 개발 담당했습니다.", src: "./assets/miraen_high_china_title.png", skills: "#javascript, #css, #html, #jquery", href: ["https://miraen-contents-exported.vercel.app/high/china/contents/lesson02/ops/2/2_01_04.html", "https://miraen-contents-exported.vercel.app/high/china/contents/lesson06/ops/6/6_01_08.html", "https://miraen-contents-exported.vercel.app/high/china/contents/lesson07/ops/7/7_01_18.html"], githref: "https://github.com/Dalguiee/miraen_contents_exported" },
-    { id: 14, expired: false, name: "CGN 통합페이지", desc: "2025년 2월 20주년 기념 CGN 통합페이지 리뉴얼을 맡아 작업하였습니다. 현재는 운영관리중에 있습니다.", src: "./assets/cgn_intergration.png", skills: "#react.js, #tailwind, #vite, #nest.js", href: ["https://cgn-intergration-exported.vercel.app/"], githref: "https://github.com/Dalguiee/cgn_intergration_exported" },
-  ];
 
   const srcEdit = (src) => {
     setWorkSrc(src);
@@ -66,7 +48,7 @@ function Section3() {
             >
               <div className="text">
                 <strong className={`${item.expired ? "line-through" : ""}`}>{item.name}</strong>
-                <span className={`${item.expired ? "line-through" : ""}`}>{item.desc}</span>
+                <span className={`${item.expired ? "line-through" : ""}`}>{parse(item.desc)}</span>
                 <p className={`${item.expired ? "line-through" : ""}`}>{item.skills}</p>
               </div>
               <div className="flex-col gap-[10px] links">
---
+++ app/datas/skills.js
@@ -0,0 +1,36 @@
+export const skillbox1 = [
+  { id: 1, group: "box1", src: "./assets/js.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 다룹니다. 함수형 코드에 강점을 가지고 있습니다.", name: "JavaScript", active: false },
+  { id: 2, group: "box1", src: "./assets/css.png", text: "도큐먼트 디자인을 위한 CSS코드를 다루며 시각적인 디자인 대부분을 구축할 수 있습니다.", name: "CSS3", active: false },
+  { id: 3, group: "box1", src: "./assets/scss.png", text: "심화된 CSS코드인 SCSS를 더 주로 다룹니다.", name: "SCSS", active: false },
+  { id: 4, group: "box1", src: "./assets/html.png", text: "도큐먼트 구조파악과 페이지의 마크업을 위한 기초적 구조를 효과적으로 구축합니다.", name: "HTML5", active: false },
+  { id: 5, group: "box1", src: "./assets/jquery.png", text: "자바스크립트에 더해 다룰 줄 아는 가장 기초적인 라이브러리입니다. HTML 로 개발하였던 교육컨텐츠 개발기술로 주로 사용하였습니다.", name: "jQuery", active: false },
+  { id: 6, group: "box1", src: "./assets/react.png", text: "저의 주력 프론트 프레임워크이며 자바스크립트에 대한 이해도와 더불어 컴포넌트 구조와 상황에 따른 분기점 활용에 관하여 자신 있습니다. 또한 SPA 페이징구조를 파악하고 있으며 실무를 포함한 여러 프로젝트를 수행하였습니다.", name: "React.js", active: false },
+  { id: 30, group: "box1", src: "./assets/reactnative.svg", text: "리액트를 다루던 기술을 기반으로 본래 진행했던 플랫폼 프로젝트의 사이드 프로젝트로 Expo 와 React-Native 를 통한 어플리케이션으로의 확장 경험이 있습니다. 기존 반응형으로 제작된 플랫폼의 WebView-Import 스타일부터, 리액트 네이티브 자체 퍼블리싱 및 개발을 통해 개발까지 진행할 수 있습니다.", name: "React-Native", active: false },
+  { id: 7, group: "box1", src: "./assets/typescript.png", text: "소규모 프로젝트에선 회의적이나 대규모 프로젝트에서 안정성 구비를 위해 사용합니다.", name: "TypeScript", active: false },
+  { id: 8, group: "box1", src: "./assets/next.png", text: "React.js에 대한 이해도와 더불어 서버명령과 엔드포인트 구축을 다루는데에 특히 이해도가 높습니다.", name: "Next.js", active: false },
+  { id: 9, group: "box1", src: "./assets/vue.png", text: "기본적인 컨텍스트(store) 이해와 더불어 vue의 각종 독립 훅을 이해하고 능동적으로 다룰 수 있습니다.", name: "Vue.js", active: false },
+  { id: 28, group: "box1", src: "./assets/tailwind.svg", text: "기본적인 CSS 스타일뿐만 아니라 React.js 에서의 state, 혹은 각종 value와 boolean 에 따른 분기형 동적스타일에 능합니다.", name: "Tailwind CSS", active: false },
+];
+export const skillbox2 = [
+  { id: 10, group: "box2", src: "./assets/mysql.png", text: "쿼리명령을 기본적으로 다루며 프로젝트로 여러 데이터 교환 구조를 만들어 본 경험이 있습니다.", name: "mySQL", active: false },
+  { id: 11, group: "box2", src: "./assets/express.png", text: "엔드포인트가 내장된 Next.js, Nuxt.js 를 제외한 프로젝트에서 백엔드 서버 구성을 위해 다수 다룹니다.", name: "Express.js", active: false },
+  { id: 12, group: "box2", src: "./assets/npm.png", text: "프레임워크에서 각종 모듈을 위한 기초적인 모듈로써 모든 구조에서 응용하고 있습니다.", name: "NPM", active: false },
+  { id: 13, group: "box2", src: "./assets/mongodb.png", text: "현재 사용하는 주요 데이터베이스이며 독자규격의 쿼리명령과 더불어 풀스택 프로젝트에 사용한 경험 있습니다.", name: "MongoDB", active: false },
+  { id: 14, group: "box2", src: "./assets/mariadb.png", text: "mySQL 쿼리명령문과 을 기본적으로 다루며 오라클의 데이터베이스 구성을 손쉽게 사용하기 위해 주로 이용하고 있습니다.", name: "MariaDB", active: false },
+  { id: 15, group: "box2", src: "./assets/nodejs.png", text: "서버구성을 위하여 필수적으로 다루며 주로 Express.js 프레임워크로 사용하고 있습니다.", name: "Node.js", active: false },
+  { id: 29, group: "box2", src: "./assets/socketio.png", text: "실시간 websocket 을 위한 가장 강력한 라이브러리인 socket.io 를 통하여 회사 내 사설블라인드 앱을 만드는 사이드프로젝트를 고안하였습니다. 뿐만아닌, 회사내부 서버컴퓨터의 연결여부를 인트라넷 외부에서 테스트하기 위한 테스트코드도 작성하여 테스트한 바 있습니다.", name: "Socket.io", active: false },
+];
+export const skillbox3 = [
+  { id: 16, group: "box3", src: "./assets/github.png", text: "각종 프로젝트 버전 공유와 업데이트를 위해 사용할 줄 알고있습니다.", name: "GitHub", active: false },
+  { id: 17, group: "box3", src: "./assets/cloudtype.png", text: "제한적이지만 동적서버를 제공하는 사이트로 몇몇 프로젝트 서버를 위해 사용중입니다.", name: "CloudType", active: false },
+  { id: 18, group: "box3", src: "./assets/postman.png", text: "각종 API들을 사용하기 앞서 사용자 키와 각종 파라미터를 테스트하는데 사용하였습니다.", name: "PostMan", active: false },
+  { id: 19, group: "box3", src: "./assets/swiper.png", text: "페이지 전환이라는 요소에서 횡스크롤 디자인과 기능적으로 가장 이상적인 플러그인 이라고 생각합니다.", name: "Swiper.js", active: false },
+  { id: 20, group: "box3", src: "./assets/fullpage.png", text: "종스크롤 페이지 전환에서 주로 사용하는 플러그인입니다.", name: "Fullpage.js", active: false },
+  { id: 21, group: "box3", src: "./assets/figma.png", text: "페이지 사전구상에서 가장 기초적으로 사용하고 있으며 기본적인 디자인 구조를 만들 수 있습니다.", name: "Figma", active: false },
+  { id: 22, group: "box3", src: "./assets/zeplin.png", text: "포토샵과 연결하여 기초적인 사이트 디자인을 구상하는데 사용할 수 있어요.", name: "Zeplin", active: false },
+  { id: 23, group: "box3", src: "./assets/vercel.png", text: "next.js 프로젝트 빌드와 업로드를 위해 사용중입니다.", name: "Vercel", active: false },
+  { id: 24, group: "box3", src: "./assets/netlify.png", text: "vue 프로젝트 빌드와 업로드를 위해 사용중입니다.", name: "Netlify", active: false },
+  { id: 25, group: "box3", src: "./assets/notion.png", text: "각종 중요한 코드 메모와 팀 프로젝트 상황공유를 위해 사용하고 있으며 마크다운 명령어를 주로 다룰 수 있어요.", name: "Notion", active: false },
+  { id: 26, group: "box3", src: "./assets/finale.png", text: "각종 음악악보 사보 및 작곡, 가상악기로 음원제작을 할 수 있어요.", name: "Make Music Finale", active: false },
+  { id: 27, group: "box3", src: "./assets/german.png", text: "독일에서의 유학생활중 학업과 현업으로 독일어 회화가 가능합니다.", name: "Deutsch Sprach", active: false },
+];
