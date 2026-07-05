73971d897d6a67dfe20a95906b1babb81e234508
Sovidi <lim132445@gmail.com>
2026-07-05 21:39:35 +0900

    달달영어 추가

--- app/components/Section1.js
+++ app/components/Section1.js
@@ -1,37 +1,37 @@
-import React, { useContext, useEffect } from "react";
-import { myContext } from "./Context";
-import ReactFullpage from "@fullpage/react-fullpage";
+import React, { useContext, useEffect } from 'react';
+import { myContext } from './Context';
+import ReactFullpage from '@fullpage/react-fullpage';
 
 function Section1() {
   return (
-    <section className="section section1">
-      <div className="sec1">
-        <div className="textbox">
+    <section className='section section1'>
+      <div className='sec1'>
+        <div className='textbox'>
           <span>프론트엔드 개발자</span>
           <strong>CHAEMIN LIM</strong>
         </div>
       </div>
-      <div className="sec2">
-        <span className="birth">1992.04.29</span>
-        <figure className="finderlogo">
-          <img src="./assets/finder.png" />
+      <div className='sec2'>
+        <span className='birth'>1992.04.29</span>
+        <figure className='finderlogo'>
+          <img src='./assets/finder.png' />
         </figure>
-        <div className="textbox">
-          <figure className="pointlogo">
-            <img src="./assets/textpoint.png" />
+        <div className='textbox'>
+          <figure className='pointlogo'>
+            <img src='./assets/textpoint.png' />
           </figure>
-          <span className="firsttext">
+          <span className='firsttext'>
             <b>프론트엔드</b> 개발, UI/UX,
             <br />
             사용자 경험 <b>최적화,</b>
             <br />
-            <b>백엔드</b>개발 및 협업
+            <b>백엔드</b>와의 효율적 협업
             <br />
             <b>컨텐츠</b> 및 <b>플랫폼</b>
             <br />
-            <b>어플</b>리케이션
+            <b>어플</b>리케이션 프로덕션
           </span>
-          <span className="secondtext">프론트엔드 영역으로 시작하여, UI/UX 개발자를 지나 현재는 어플리케이션과 백엔드까지 모든것을 소화하고자 합니다. 완벽한 웹 개발을 지향하고 항상 발전하기 위해 궁금증을 꺼뜨리지 않고 개발중에 있습니다.</span>
+          <span className='secondtext'>프론트엔드 영역으로 시작하여, UI/UX 개발자를 지나 현재는 어플리케이션과 백엔드까지 모든것을 소화하고자 합니다. 완벽한 웹 개발을 지향하고 항상 발전하기 위해 궁금증을 꺼뜨리지 않고 개발중에 있습니다.</span>
         </div>
       </div>
     </section>
--- app/components/Section2.js
+++ app/components/Section2.js
@@ -1,41 +1,41 @@
-import React, { useEffect, useState } from "react";
-import Section2_groups from "./section2_groups";
-import Section2_card_detail from "./section2_card_detail";
-import { skillbox1, skillbox2, skillbox3 } from "../datas/skills";
+import React, { useEffect, useState } from 'react';
+import Section2_groups from './section2_groups';
+import Section2_card_detail from './section2_card_detail';
+import { skillbox1, skillbox2, skillbox3 } from '../datas/skills';
 
 function Section2() {
   const [tagId, setTagId] = useState();
   const [selectedItem, setSelectedItem] = useState({});
 
   const idErase = () => {
-    setTagId("");
+    setTagId('');
   };
 
   const idEdit = (e, str) => {
-    e.target.classList.contains("popclose") ? idErase() : setTagId(str);
-    console.log(e.target.classList.contains("popclose"));
+    e.target.classList.contains('popclose') ? idErase() : setTagId(str);
+    console.log(e.target.classList.contains('popclose'));
   };
 
   return (
-    <section className="section section2">
-      <div className="sec1" onClick={idErase}>
-        <div className="textbox">
+    <section className='section section2'>
+      <div className='sec1' onClick={idErase}>
+        <div className='textbox'>
           <figure>
             <figcaption>ABOUT</figcaption>
-            <img src="../assets/about.png" />
+            <img src='../assets/about.png' />
           </figure>
           <span>다수의 개발 경험으로 말미암아 광범위하게 접근 및 해결책을 제시하는 개발자가 되고자 합니다. 프론트엔드 영역에서 손에 주어진 프로젝트를 성공적으로 수행해 왔으며, 개발업무와 R&D를 항상 병행합니다. 웹 애플리케이션부터 모바일 앱, 데스크톱 애플리케이션, 그리고 나아가 백엔드 데이터베이스와 서버, 보안 시스템까지 여러 영역에서 다양한 또한 소프트웨어 개발 트렌드와 도구를 최신으로 유지하고, 개발 프로세스를 개선하기 위해 지속적인 학습과 실험은 아직 멈추지 않았습니다.</span>
         </div>
       </div>
-      <div className="overflow-hidden sec2">
-        <span className="scrolltext">JAVASCRIPT CSS REACT SCSS HTML JQUERY REACT TYPESCRIPT NEXT.JS VUE.JS MYSQL NPM MONGODB NOSQL NODE.JS</span>
-        <span className="clickIcon fa-shake">
+      <div className='sec2 overflow-hidden'>
+        <span className='scrolltext'>JAVASCRIPT CSS REACT SCSS HTML JQUERY REACT TYPESCRIPT NEXT.JS VUE.JS MYSQL NPM MONGODB NOSQL NODE.JS</span>
+        <span className='clickIcon fa-shake'>
           <div></div>
         </span>
-        <div className="skillIntroduce">
-          <Section2_groups className={selectedItem?.group === "box1" ? "z-[10]" : ""} skillbox={skillbox1} setSelectedItem={setSelectedItem} tagId={tagId} idEdit={idEdit} />
-          <Section2_groups className={selectedItem?.group === "box2" ? "z-[10]" : ""} skillbox={skillbox2} setSelectedItem={setSelectedItem} tagId={tagId} idEdit={idEdit} />
-          <Section2_groups className={selectedItem?.group === "box3" ? "z-[10]" : ""} skillbox={skillbox3} setSelectedItem={setSelectedItem} tagId={tagId} idEdit={idEdit} />
+        <div className='skillIntroduce'>
+          <Section2_groups className={selectedItem?.group === 'box1' ? 'z-[10]' : ''} skillbox={skillbox1} setSelectedItem={setSelectedItem} tagId={tagId} idEdit={idEdit} skillsCategory={`Front-End`} />
+          <Section2_groups className={selectedItem?.group === 'box2' ? 'z-[10]' : ''} skillbox={skillbox2} setSelectedItem={setSelectedItem} tagId={tagId} idEdit={idEdit} skillsCategory={`Back-End`} />
+          <Section2_groups className={selectedItem?.group === 'box3' ? 'z-[10]' : ''} skillbox={skillbox3} setSelectedItem={setSelectedItem} tagId={tagId} idEdit={idEdit} skillsCategory={`ETC`} />
         </div>
       </div>
       {/* <Section2_card_detail item={selectedItem} /> */}
