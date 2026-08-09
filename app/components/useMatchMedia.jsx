db968e2db21985c7c1fbf9df38ea9432d32a132e
Sovidi <lim132445@gmail.com>
2026-08-09 21:34:18 +0900

    반응형 모듈 성능이슈 수정

--- app/components/useMatchMedia.jsx
+++ app/components/useMatchMedia.jsx
@@ -1,18 +1,26 @@
-import { useSyncExternalStore } from 'react';
+import { useCallback, useSyncExternalStore } from 'react';
 
 const useMatchMedia = query => {
-  const subscribe = callback => {
-    const media = window.matchMedia(query);
-    media.addEventListener('change', callback);
+  const subscribe = useCallback(
+    callback => {
+      const media = window.matchMedia(query);
+      media.addEventListener('change', callback);
 
-    return () => media.removeEventListener(`change`, callback);
-  };
-  const getSnapshot = () => {
+      return () => media.removeEventListener(`change`, callback);
+    },
+    [query]
+  );
+
+  const getSnapshot = useCallback(() => {
     console.log(`현재 ${query}에 대한 반응형 변경 감지됨.`);
     return window.matchMedia(query).matches;
-  };
+  }, [query]);
 
-  return useSyncExternalStore(subscribe, getSnapshot);
+  const getServerSnapshot = useCallback(() => {
+    return false;
+  }, []);
+
+  return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
 };
 
 export default useMatchMedia;
